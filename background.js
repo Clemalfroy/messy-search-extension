f9f721f784158f4f2a480bca5ca64a900157043c
daggerron <cmalfroy@student.42.fr>
2021-04-07 21:03:29 +0200

    v0

---
+++ background.js
@@ -0,0 +1,144 @@
+const getFromLocalStorageAsync = async (keys) => {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(keys, resolve)
+    })
+}
+
+const queryTabs = async (queryObject) => {
+    return new Promise((resolve) => chrome.tabs.query(queryObject, resolve));
+}
+
+const moveToTab = async ({ tab }) => {
+    return new Promise((resolve) => chrome.tabs.update(tab.id, {
+        active: true
+    }, resolve))
+}
+
+const getAllTabsInWindow = async () => {
+    return queryTabs({
+        currentWindow: true
+    });
+}
+
+const getActiveTabInWindow = async () => {
+    return queryTabs({
+        currentWindow: true,
+        active: true
+    });
+}
+
+const getAllTabsFromLocalStorage = async () => {
+    const tabs = await getAllTabsInWindow()
+    const tabIdsString = tabs.map(t => t.id.toString(10))
+    return getFromLocalStorageAsync(tabIdsString)
+}
+
+
+const removeFromLocalStorageAsync = async (keys) => {
+    return new Promise((resolve) => {
+        chrome.storage.local.remove(keys, resolve)
+    })
+}
+
+const setToLocalStorageAsync = async (items) => {
+    return new Promise((resolve) => {
+        chrome.storage.local.set(items, resolve)
+    })
+}
+
+const removeTabInfoFromLocalStorage = async (tabId) => {
+    await removeFromLocalStorageAsync(tabId.toString(10))
+    console.log(`Tab ${tabId}: removed from local storage`);
+}
+
+
+const getTabInfoFromLocalStorage = async (tabId) => {
+    return getFromLocalStorageAsync(tabId.toString(10))
+}
+
+const executeScriptAsync = async (params) => {
+    return new Promise((resolve) => {
+        chrome.scripting.executeScript(params, resolve);
+    })
+}
+
+const getSanitizedBodyText = () => {
+    const rawText = document.body.innerText.toLowerCase()
+    const wordSet = new Set(rawText.split(/\s+/))
+    return Array.from(wordSet).join(' ')
+}
+
+const addTabInfoToLocalStorage = async (tab) => {
+    const tabId = tab.id
+    const tabText = await getTextFromTab(tabId)
+    await setToLocalStorageAsync({
+        [tabId]: {...tab, tabText}
+    })
+    console.log(`Tab ${tabId}: added to local storage`);
+}
+
+const getTextFromTab = async (tabId) => {
+    const [{ result }] = await executeScriptAsync({
+        target: { tabId },
+        function: getSanitizedBodyText
+    }); 
+    return result
+}
+
+const sendMessageAsync = async (tabId, action) => {
+    return new Promise((resolve) => {
+        chrome.tabs.sendMessage(tabId, { action }, resolve);
+    })
+}
+
+const searchCommandHandler = async () => {
+    const [activeTab] = await getActiveTabInWindow()
+    await sendMessageAsync(activeTab.id, 'search-handler')
+}
+
+const commandDispatcher = {
+    "search": searchCommandHandler
+}
+
+const messageActionDispatcher = {
+    "getAllTabsFromLocalStorage": getAllTabsFromLocalStorage,
+    "moveToTab": moveToTab
+}
+
+// Listeners
+
+chrome.runtime.onInstalled.addListener(async () => {
+    const isValidTab = (tab) => tab.url.startsWith('http')
+    const allTabs = (await getAllTabsInWindow())
+    const validTabs = allTabs.filter(isValidTab)
+    for (const tab of validTabs) {
+        await addTabInfoToLocalStorage(tab)    
+    }
+  });
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
+        await removeTabInfoFromLocalStorage(tabId);
+        await addTabInfoToLocalStorage(tab)    
+    }
+})
+
+chrome.tabs.onRemoved.addListener(async (tabId) => {
+    await removeTabInfoFromLocalStorage(tabId)
+})
+
+chrome.commands.onCommand.addListener((command) => {
+    if (Object.keys(commandDispatcher).includes(command)) {
+        commandDispatcher[command]()
+    }
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    const { action, ...options } = message;
+    if (Object.keys(messageActionDispatcher).includes(action)) {
+        messageActionDispatcher[action](options).then(
+            returnValue => sendResponse(returnValue)
+        )
+        return true
+    }
+})
---
+++ content-script.js
@@ -0,0 +1,125 @@
+
+const getAllTabsFromLocalStorage = async () => {
+    return new Promise((resolve) => {
+        chrome.runtime.sendMessage({ action: "getAllTabsFromLocalStorage" }, (response) => {
+            resolve(response)
+        })  
+    })
+}
+
+const queryHasMatched = (tab, query) => {
+    return (tab.tabText && tab.tabText.includes(query)) || tab.title.toLowerCase().includes(query)
+}
+
+const search = async (query) => {
+    const sanitizedQuery = query.toLowerCase()
+    const localStorageTabs = await getAllTabsFromLocalStorage()
+    const foundTabs = Object.values(localStorageTabs).filter(
+        t => queryHasMatched(t, sanitizedQuery)
+    )
+    return foundTabs
+}
+
+const handleTabChange = (tab) => {
+    toggleSearchBar()
+    chrome.runtime.sendMessage({ action: "moveToTab", tab })  
+}
+
+const displaySearchResults = (tabResults) => {
+    const searchResultsContainer = document.querySelector('#messy-search-results-container')
+    searchResultsContainer.innerHTML = "";
+    tabResults.forEach((tab, i) => {
+        const tabDiv = document.createElement('div')
+        tabDiv.dataset.tabId = tab.id
+        tabDiv.tabIndex = 0;
+        tabDiv.classList.add('tab-result')
+        const logoImg = document.createElement('img')
+        logoImg.src = tab.favIconUrl;
+        const tabDivTitle = document.createElement('div')
+        tabDivTitle.classList.add('tab-div-title')
+        const tabTitle = document.createElement('p')
+        const tabIndex = document.createElement('div')
+        tabTitle.innerText = tab.title
+        tabIndex.innerText = `${i + 1}`
+        tabIndex.classList.add('tab-result-index')
+        tabDiv.insertAdjacentElement('beforeend', tabDivTitle)
+        tabDivTitle.insertAdjacentElement('beforeend', tabIndex)
+        tabDivTitle.insertAdjacentElement('beforeend', tabTitle)
+        tabDiv.insertAdjacentElement('beforeend', logoImg)
+        searchResultsContainer.insertAdjacentElement('beforeend', tabDiv)
+        tabDiv.addEventListener("keydown", (e) => {
+            if (e.key === 'Enter') handleTabChange(tab)
+        })
+    })
+}
+
+const moveToNthTabResult = (n) => {
+    const tabResults = Array.from(document.querySelectorAll('#messy-search-results-container .tab-result'))
+    const selectedTab = tabResults[n]
+    if (selectedTab) selectedTab.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter' }))
+}
+
+const handleSearch = async (e) => {
+    if (e.key === "Enter") {
+        moveToNthTabResult(0)
+    }
+    if (e.altKey && e.code.includes('Digit')) {
+        e.preventDefault()
+    }
+    const searchInputElement = document.querySelector('#messy-search-input')
+    const query = searchInputElement.value
+    if (query.length > 0) {
+        const foundTabs = await search(query)
+        displaySearchResults(foundTabs)
+    }
+
+}
+
+const createSearchBar = () => {
+    const searchContainer = document.createElement('div')
+    searchContainer.id = 'messy-search-container'
+    const searchInput = document.createElement('input')
+    searchInput.id = "messy-search-input"
+    searchInput.onkeydown = handleSearch
+    const searchResultsContainer = document.createElement('div')
+    searchResultsContainer.id = 'messy-search-results-container'
+    searchContainer.insertAdjacentElement('beforeend', searchInput);
+    searchContainer.insertAdjacentElement('beforeend', searchResultsContainer)
+    document.body.insertAdjacentElement('beforeend', searchContainer);
+    searchInput.focus()
+}
+
+
+const destroySearchBar = (searchBar) => {
+    searchBar.remove()
+}
+
+const toggleSearchBar = () => {
+    const searchBarElement = document.querySelector('#messy-search-container')
+    searchBarElement ? destroySearchBar(searchBarElement) : createSearchBar()
+}
+
+const actionDispatcher = {
+    'search-handler': toggleSearchBar
+}
+
+document.body.addEventListener('click', (e) => {
+    const isExtensionElement = e.path.map(el => el.id).some((i) => i && i.includes('messy'))
+    const searchBarElement = document.querySelector('#messy-search-container')
+    if (searchBarElement && !isExtensionElement) destroySearchBar(searchBarElement);
+})
+
+document.addEventListener('keydown', function (event) {
+    const searchBarElement = document.querySelector('#messy-search-container')
+    if (searchBarElement && event.altKey && event.code.includes('Digit')) {
+        const digitKey = parseInt(event.code.replace('Digit', ''), 10)
+        moveToNthTabResult(digitKey - 1)
+    }
+});
+
+chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
+    const { action } = msg
+    if (Object.keys(actionDispatcher).includes(action)) {
+        actionDispatcher[action]()
+    }
+});
